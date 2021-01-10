92619525f91a5f6b027afcf8911418cc3634f8d6
Haruka Kai <plalal@gmail.com>
2021-01-10 17:59:43 +0900

    library

---
+++ lib/lib.scm
@@ -0,0 +1,25 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; self-made library
+
+;;; I/O helper
+
+(define (line-read :optional [reader read])
+  (with-input-from-string (read-line) reader))
+
+(define line-read$ (pa$ pa$ line-read))
+
+(define (replist n proc)
+  (let loop [[n n]
+             [acc '()]
+             ]
+    (if (zero? n)
+      (reverse acc)
+      (loop (- n 1) (cons (proc) acc)))))
+
+(define replist$ (pa$ pa$ replist))
+
+(define (read-matrix rows cols :optional [reader read-char])
+  (replist rows (line-read$ (replist$ cols reader))))
+
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;

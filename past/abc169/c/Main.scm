daf3c4334d2a383259847aa22efa61fc5fa7fdf6
Haruka Kai <plalal@gmail.com>
2021-01-11 05:21:03 +0900

    abc169 a b c

---
+++ past/abc169/c/Main.scm
@@ -0,0 +1,23 @@
+;;; common libs
+(use srfi-1) (use util.match)
+;;; self-made library: I/O helper
+(define (line-read :optional [reader read]) (with-input-from-string (read-line) reader))
+(define line-read$ (pa$ pa$ line-read))
+(define (replist n proc) (let loop [[n n] [acc '()] ] (if (zero? n) (reverse acc) (loop (- n 1) (cons (proc) acc)))))
+(define replist$ (pa$ pa$ replist)) 
+(define (read-matrix rows cols :optional [reader read-char]) (replist rows (line-read$ (replist$ cols reader))))
+;;; entry point
+(define (main _) ((.$ emit solve parse)) 0)
+;;; solution body
+
+(define (parse)
+  (let* [[A (read)]
+         [B (read)]
+         ]
+    (values A (round->exact (* B 100)))))
+
+(define (solve A B)
+  (floor->exact (* A B 1/100))
+  )
+
+(define emit print)

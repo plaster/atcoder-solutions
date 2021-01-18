4380033bff40c6c8348cf9ae5a3aa7f753c0d1f2
Haruka Kai <plalal@gmail.com>
2021-01-19 03:19:33 +0900

    judge-update-202004 a b c

---
+++ past/judge-update-202004/b/Main.scm
@@ -0,0 +1,31 @@
+;;; common libs
+(use srfi-1) (use util.match)
+;;; self-made library: I/O helper
+(define (line-read :optional [reader read]) (with-input-from-string (read-line) reader))
+(define line-read$ (pa$ pa$ line-read))
+(define (replist n proc) (let loop [[n n] [acc '()] ] (if (zero? n) (reverse acc) (loop (- n 1) (cons (proc) acc)))))
+(define replist$ (pa$ pa$ replist)) 
+(define (read-matrix rows cols :optional [reader read-char]) (replist rows (line-read$ (replist$ cols reader))))
+;;; entry point
+(define (main _) ((.$ emit solve parse)) 0)
+;;; solution body
+
+(define (parse)
+  (let1 N (line-read)
+    (read-matrix N 2 read)
+    ))
+
+(use srfi-42)
+
+(define (solve xcs)
+  (let [[ rs (list-ec (:list xc xcs)
+                      (if (eq? 'R (cadr xc)))
+                      (car xc))]
+        [ bs (list-ec (:list xc xcs)
+                      (if (eq? 'B (cadr xc)))
+                      (car xc))]
+        ]
+    (append (sort rs) (sort bs))
+    ))
+
+(define emit (for-each$ print))
---
+++ past/judge-update-202004/c/Main.scm
@@ -0,0 +1,30 @@
+;;; common libs
+(use srfi-1) (use util.match)
+;;; self-made library: I/O helper
+(define (line-read :optional [reader read]) (with-input-from-string (read-line) reader))
+(define line-read$ (pa$ pa$ line-read))
+(define (replist n proc) (let loop [[n n] [acc '()] ] (if (zero? n) (reverse acc) (loop (- n 1) (cons (proc) acc)))))
+(define replist$ (pa$ pa$ replist)) 
+(define (read-matrix rows cols :optional [reader read-char]) (replist rows (line-read$ (replist$ cols reader))))
+;;; entry point
+(define (main _) ((.$ emit solve parse)) 0)
+;;; solution body
+
+(define (parse)
+  (apply values (replist 3 read))
+  )
+
+(define (solve a b c)
+  (cond
+    [ (not (>= a b c 0))
+     0 ]
+    [ (= 0 a b c)
+     1 ]
+    [else
+      (+ (solve a b (- c 1))
+         (solve a (- b 1) c)
+         (solve (- a 1) b c)
+         ) ]
+    ))
+
+(define emit print)

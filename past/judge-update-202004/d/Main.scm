ade6d1bbb54b788bcdec1b342bc08e3d6cb7f424
Haruka Kai <plalal@gmail.com>
2021-01-20 04:11:33 +0900

    although accelerated by binary search, d still TLE

--- past/judge-update-202004/d/Main.scm
+++ past/judge-update-202004/d/Main.scm
@@ -16,19 +16,63 @@
          [ As (replist N read) ]
          [ Ss (replist Q read) ]
          ]
-    (values As Ss)))
+    (values (gcded As) Ss)))
 
-(use gauche.sequence)
+(define (gcded As)
+  (let loop [[ As As]
+             [ G #f ]
+             [ Gs '() ]]
+    (match As
+      [ () ($ list->vector $ reverse Gs) ]
+      [ (A . As)
+       (let1 G (if G (gcd G A) A)
+         (loop As G (cons G Gs)))
+       ])))
+
+(define (first-index-binary n pred)
+  (let1 di (let f [[di 1]]
+             (if (< di n)
+               (f (ash di 1))
+               di))
+    (let loop [[i 0] [iprev #f] [di di]]
+      (let1 di (ash di -1)
+        (cond
+          [ (not (< i n))
+           (if (zero? di)
+             iprev
+             (loop (- i di) iprev di)) ]
+          [ (negative? i) ;; reached here iff (pred 0)
+           iprev ]
+          [ (pred i)
+           (if (zero? di)
+             i
+             (loop (- i di) i di)) ]
+          [else
+            (if (zero? di)
+              iprev
+              (loop (+ i di) iprev di)) ]
+          )))))
+; test code
+;(use srfi-42)
+;(define (t n r) (first-index-binary n (pa$ <= r)))
+;(do-ec (: n 0 1000) (: r 0 1000) 
+;       (:let x (t n r))
+;       (or (if (> n r) (and x (= r x)) (not x))
+;           (errorf "~s" (vector n r x))))
+
 
 (define ((%solve$ As) S)
-  (let/cc return
-    (fold-with-index
-      (^ (i A X)
-         (let1 X (gcd X A)
-           (if (= 1 X)
-             ($ return $ + 1 i))
-           X))
-      S As)))
+  (let1 i (first-index-binary (vector-length As)
+                              (.$ (pa$ = 1)
+                                  (pa$ gcd S)
+                                  (pa$ vector-ref As))
+                              )
+    (if i (+ i 1)
+      ($ gcd S
+         $ vector-ref As
+         $ + -1
+         $ vector-length As ))))
+
 
 (define (solve As Ss)
   (map (%solve$ As) Ss)

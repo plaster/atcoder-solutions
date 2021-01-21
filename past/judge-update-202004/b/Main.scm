6be3b78cac4d1079cc321791faf34ca364c3756c
Haruka Kai <plalal@gmail.com>
2021-01-22 02:54:54 +0900

    update template

--- past/judge-update-202004/b/Main.scm
+++ past/judge-update-202004/b/Main.scm
@@ -1,11 +1,11 @@
 ;;; common libs
 (use srfi-1) (use util.match)
-;;; self-made library: I/O helper
+;;; I/O helper
 (define (line-read :optional [reader read]) (with-input-from-string (read-line) reader))
-(define line-read$ (pa$ pa$ line-read))
-(define (replist n proc) (let loop [[n n] [acc '()] ] (if (zero? n) (reverse acc) (loop (- n 1) (cons (proc) acc)))))
-(define replist$ (pa$ pa$ replist)) 
-(define (read-matrix rows cols :optional [reader read-char]) (replist rows (line-read$ (replist$ cols reader))))
+(define ((line-read$ reader)) (line-read reader))
+(define (replist n proc) (unfold zero? (^ (_) (proc)) (pa$ + -1) n))
+(define ((replist$ n proc)) (replist n proc))
+(define (read-matrix rows cols :optional [reader read]) (replist rows (line-read$ (replist$ cols reader))))
 ;;; entry point
 (define (main _) ((.$ emit solve parse)) 0)
 ;;; solution body

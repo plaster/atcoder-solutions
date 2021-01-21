97b469a3bf187d4679b6f787f772d877e5b33263
Haruka Kai <plalal@gmail.com>
2021-01-22 04:52:14 +0900

    update template

--- templates/source/Main.scm
+++ templates/source/Main.scm
@@ -1,9 +1,9 @@
 ;;; common libs
-(use srfi-1) (use util.match)
+(use srfi-1) (use util.match) (use gauche.generator)
 ;;; I/O helper
 (define (line-read :optional [reader read]) (with-input-from-string (read-line) reader))
 (define ((line-read$ reader)) (line-read reader))
-(define (replist n proc) (unfold zero? (^ (_) (proc)) (pa$ + -1) n))
+(define (replist n proc) ($ generator->list $ gunfold zero? (^ (_) (proc)) ($ + -1 $) n))
 (define ((replist$ n proc)) (replist n proc))
 (define (read-matrix rows cols :optional [reader read]) (replist rows (line-read$ (replist$ cols reader))))
 ;;; entry point

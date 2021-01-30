9b0d51a3d1ed23ae58ae8d6cac0c46a619cd1ea6
Haruka Kai <plalal@gmail.com>
2021-01-30 20:37:19 +0900

    split templates

---
+++ templates/lib/io.scm
@@ -0,0 +1,6 @@
+;;; I/O helper
+(define (line-read :optional [reader read]) (with-input-from-string (read-line) reader))
+(define ((line-read$ reader)) (line-read reader))
+(define (replist n proc) ($ generator->list $ gunfold zero? (^ (_) (proc)) ($ + -1 $) n))
+(define ((replist$ n proc)) (replist n proc))
+(define (read-matrix rows cols :optional [reader read]) (replist rows (line-read$ (replist$ cols reader))))
--- templates/source/Main.scm
+++ templates/source/Main.scm
@@ -1,13 +1,9 @@
 ;;; common libs
 (use srfi-1) (use util.match) (use gauche.generator)
-;;; I/O helper
-(define (line-read :optional [reader read]) (with-input-from-string (read-line) reader))
-(define ((line-read$ reader)) (line-read reader))
-(define (replist n proc) ($ generator->list $ gunfold zero? (^ (_) (proc)) ($ + -1 $) n))
-(define ((replist$ n proc)) (replist n proc))
-(define (read-matrix rows cols :optional [reader read]) (replist rows (line-read$ (replist$ cols reader))))
 ;;; entry point
-(define (main _) ((.$ emit solve parse)) 0)
+(define (main _)
+  (call-with-values (cut call-with-values parse solve)
+    emit) 0)
 ;;; solution body
 
 (define (parse)

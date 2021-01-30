e3ec98221d143814333dedcb98f54d1daa92dc40
Haruka Kai <plalal@gmail.com>
2021-01-31 01:45:46 +0900

    abc185 b ac

--- past/abc185/b/Main.scm
+++ past/abc185/b/Main.scm
@@ -1,5 +1,5 @@
 ;;; common libs
-(use srfi-1) (use util.match) (use gauche.generator)
+(use util.match)
 ;;; entry point
 (define (main _)
   (call-with-values (cut call-with-values parse solve)
@@ -12,28 +12,30 @@
          [M (read)]
          [T (read)]
          ]
-    (values N T
-            (list-ec (: _ M)
-                     (:let A (read))
-                     (:let B (read))
-                     (cons A B)))
-    ))
+    (values N
+            (append
+              (list-ec (: _ M)
+                       (:let A (read))
+                       (:let B (read))
+                       (cons A B))
+              `((,T))
+              ))))
 
-(define (solve N T ABs)
+(define (solve N ABs)
   (let loop [[ n N]
-             [ T0 T]
+             [ T0 0]
              [ ABs ABs]]
     (match ABs
-      [ ()
-       (let1 n ($ - n $ - T T0)
+      [ ((A))
+       (let1 n ($ - n $ - A T0)
          (positive? n))
        ]
       [ (( A . B ) . ABs)
        (let1 n ($ - n $ - A T0)
          (and (positive? n)
-              (solve ($ + n $ - B A)
-                     B
-                     ABs)))
+              (loop ($ min N $ + n $ - B A)
+                    B
+                    ABs)))
        ])))
 
 (define (emit x) (print (if x 'Yes 'No)))
